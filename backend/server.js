48f373e8a047546c7cbfc978340421a4d2bdf5f2
Muhammad Abdullah Shakoor <abdullahshakoor1000@outlook.com>
2024-12-28 22:10:05 +0500

    cors driving crazy

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,26 @@ const UserRoutes = require("./routes/UserRoutes");
 
 const app = express();
 connectDB();
-
+app.options("*", cors());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://task-manager-fe-virid.vercel.app",
     methods: ["POST", "GET", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(express.json());
-
+app.use((req, res, next) => {
+  console.log(`${req.method} request to ${req.url}`);
+  next();
+});
 app.use("/api/tasks", TaskRoutes);
 app.use("/api/users", UserRoutes);
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
